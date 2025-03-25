d49c62cfd72ae9a154f709cadbfcb438cea6a697
Leandro Martini <martinileandro12@gmail.com>
2025-03-24 21:32:22 -0300

    Aprendendo UPCASTING e DOWNCASTING

---
+++ POO/Herança/ContaPoupanca.java
@@ -0,0 +1,23 @@
+package POO.Herança;
+
+public class ContaPoupanca extends Conta {
+    private Double taxaJuros;
+
+    public ContaPoupanca(){ super();};
+
+    public ContaPoupanca(Integer numero, String titular, Double saldo, Double taxaJuros){
+        super(numero, titular, saldo);
+        this.taxaJuros = taxaJuros;
+    }
+
+    public Double getTaxaJuros(){
+        return this.taxaJuros;
+    }
+    public void setTaxaJuros(Double taxaJuros){
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void SaldoAtualizado(){
+        saldo += saldo * taxaJuros;
+    }
+}
--- POO/Herança/Program.java
+++ POO/Herança/Program.java
@@ -2,7 +2,30 @@ package POO.Herança;
 
 public class Program {
     public static void main(String[] args) {
-        ContaBancaria contaBancaria = new ContaBancaria();
-        contaBancaria.getNumero();
+         Conta conta = new Conta(1003,"Bob", 0.0);
+         ContaBancaria contaBancaria = new ContaBancaria(1002, "Maria", 0.0, 500.0);
+
+         // UPCASTING
+
+         Conta acc1 = contaBancaria;
+         Conta acc2 = new ContaBancaria(1003, "Bob", 0.0, 200.0);
+         Conta acc3 = new ContaPoupanca(1004, "Anna", 0.0, 0.01);
+
+         // DOWNCASTING
+
+         ContaBancaria acc4 = (ContaBancaria)acc2;
+         // ContaBancaria acc5 = (ContaBancaria)acc3; DA ERRO;
+         if(acc3 instanceof ContaBancaria){
+            ContaBancaria acc5 = (ContaBancaria)acc3;
+            acc5.emprestimo(200.0);
+            System.out.println("Empréstimo");
+         }
+         if(acc3 instanceof ContaPoupanca){
+            ContaPoupanca acc5 = (ContaPoupanca)acc3;
+            acc5.SaldoAtualizado();
+            System.out.println("SALDO ATUALIZADO");
+         }
+
+        
     }
 }
