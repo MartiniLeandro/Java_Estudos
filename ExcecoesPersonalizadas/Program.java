7b17398e8fb937c55d276ba57b2b8895b3f13c1e
Leandro Martini <martinileandro12@gmail.com>
2025-03-30 18:25:35 -0300

    Tratemento de exceção RUIM

--- ExcecoesPersonalizadas/Program.java
+++ ExcecoesPersonalizadas/Program.java
@@ -1,7 +1,52 @@
 package ExcecoesPersonalizadas;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Numero do quarto: ");
+        Integer numeroQuarto = sc.nextInt();
+        System.out.print("Check-in (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-Out (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+
+        if(!checkOut.after(checkIn)){
+            System.out.println("Erro na reserva: Chek-Out deve ser depois do Check-In");
+        }else {
+            Reservas reserva = new Reservas(numeroQuarto, checkIn,checkOut);
+            System.out.println("Reserva: " + reserva);
+
+            System.out.println();
+            System.out.println("Digite as datas para atualizar a reserva: ");
+            System.out.print("Check-in (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-Out (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            String erro = reserva.atualizarDatas(checkIn, checkOut);
+            if(erro != null){
+                System.out.println("Erro na reserva: " + erro);
+            }else {
+                System.out.println("Reserva: " + reserva);
+            }
+
+            reserva.atualizarDatas(checkIn, checkOut);
+            System.out.println("Reserva: " + reserva);
+
+
+
+        }
+
+
+
         
+        sc.close();
     }
 }
--- ExcecoesPersonalizadas/Reservas.java
+++ ExcecoesPersonalizadas/Reservas.java
@@ -1,6 +1,6 @@
 package ExcecoesPersonalizadas;
 
-import java.sql.Date;
+import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
@@ -9,11 +9,15 @@ public class Reservas {
     private Date checkIn;
     private Date checkOut;
 
-    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservas(){};
 
-    public Reservas(Integer numeroQuarto, Date checkIn, Date checkOut;)
+    public Reservas(Integer numeroQuarto, Date checkIn, Date checkOut){
+        this.numeroQuarto = numeroQuarto;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
 
     public Integer getNumeroQuarto(){
         return this.numeroQuarto;
@@ -33,13 +37,20 @@ public class Reservas {
         long diferenca = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
     }
-    public void atualizarDatas(Date checkIn, Date checkOut){
+    public String atualizarDatas(Date checkIn, Date checkOut){
+        Date agora = new Date();
+        if(checkIn.before(agora) || checkOut.before(agora)){
+            return "Erro na reserva: atualização da data da reserva deve ser futuras ";
+        }if(!checkOut.after(checkIn)){
+            return "Erro na reserva: Chek-Out deve ser depois do Check-In";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
     public String toString(){
-        return "Quarto " + numeroQuarto + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + duracao() + " dias"
+        return "Quarto " + numeroQuarto + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + duracao() + " noites";
     }
 }
