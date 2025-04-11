29bae78fcd9e246d35d45b9e52c33f5a8bc5df36
Leandro Martini <martinileandro12@gmail.com>
2025-04-11 09:52:58 -0300

    Aprendendo um pouco mais de Generisc

--- ExerciseGenerics/Program.java
+++ ExerciseGenerics/Program.java
@@ -6,17 +6,21 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         PrintService<String> pr = new PrintService<>();
+        PrintService<Integer> pr2 = new PrintService<>();
         
         System.out.print("Quantos valores? ");
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++){
             pr.addValue(sc.next());
+            pr2.addValue(sc.nextInt());;
         }
 
 
         System.out.println("First: " + pr.first());
+        System.out.println("First: " + pr2.first());
         pr.print();
+        pr2.print();
 
         
         sc.close();
