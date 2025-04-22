2028943f4d2ef5ba2f7c43607d70f3b75ac5ec4a
Leandro Martini <martinileandro12@gmail.com>
2025-04-22 07:46:02 -0300

    Departament entity class

---
+++ ProjectJDBC/src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.Departament;
+
+public class Program {
+    public static void main(String[] args) {
+        Departament dp = new Departament(1, "books");
+        System.out.println(dp);
+    }
+    
+}
