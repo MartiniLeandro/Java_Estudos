1054b077dcfa484e6b232df607f67ddf755191f7
Leandro Martini <martinileandro12@gmail.com>
2025-04-22 07:57:27 -0300

    DepartmentDao and SellerDao interfaces

--- ProjectJDBC/src/application/Program.java
+++ ProjectJDBC/src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
-import entities.Departament;
+import java.util.Date;
+
+import entities.Department;
+import entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
-        Departament dp = new Departament(1, "books");
+        Department dp = new Department(1, "books");
         System.out.println(dp);
+        Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
+        System.out.println(sl);
     }
     
 }
