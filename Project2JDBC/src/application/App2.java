01d730f37ae6b8b93b9d80768d49c75b189d94c6
Leandro Martini <martinileandro12@gmail.com>
2025-04-26 22:43:05 -0300

    Testing project JDBC

--- Project2JDBC/src/application/App2.java
+++ Project2JDBC/src/application/App2.java
@@ -8,6 +8,7 @@ import entities.Author;
 public class App2 {
     public static void main(String[] args) {
         AuthorDao authorDao = DaoFactory.createAuthorDao();
+        
         System.out.println("\n==== test 1: find AuthorById ====");
         Author author = authorDao.findById(4);
         System.out.println(author);
@@ -16,18 +17,18 @@ public class App2 {
         List<Author> allAuthors = authorDao.listAllAuthors();
         allAuthors.forEach(System.out::println);
 
-        //System.out.println("\n==== test 3: insert Author ====");
-        //Author addAuthor = new Author(null, "Hirowuki");
-        //authorDao.insertAuthor(addAuthor);
-        //System.out.println("Insered Author");
+        System.out.println("\n==== test 3: insert Author ====");
+        Author addAuthor = new Author(null, "Hirowuki");
+        authorDao.insertAuthor(addAuthor);
+        System.out.println("Insered Author");
 
-        //System.out.println("\n==== test 4: update author ====");
-        //Author newAuthor = new Author(5, "Sawano");
-        //authorDao.updateAuthor(newAuthor);
-        //System.out.println("Updated author");
+        System.out.println("\n==== test 4: update author ====");
+        Author newAuthor = new Author(7, "Sawano");
+        authorDao.updateAuthor(newAuthor);
+        System.out.println("Updated author");
 
         System.out.println("\n==== test 5: delete author ====");
-        authorDao.deleteById(2);
+        authorDao.deleteById(8);
         System.out.println("Deleted author");
     }
 }
