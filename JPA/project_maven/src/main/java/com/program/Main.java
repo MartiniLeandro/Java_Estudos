c21248345efc6db40425fb5d08233c7ac925f787
Leandro Martini <martinileandro12@gmail.com>
2025-04-27 22:19:25 -0300

    Começando config usando MAVEN

---
+++ JPA/project_maven/src/main/java/com/program/Main.java
@@ -0,0 +1,13 @@
+package com.program;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person(1, "Bob Marley", "bob@gmail.com");
+        Person p2 = new Person(1, "Erick Marley", "erick@gmail.com");
+        Person p3 = new Person(1, "Pedro Marley", "pedro@gmail.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
+}
