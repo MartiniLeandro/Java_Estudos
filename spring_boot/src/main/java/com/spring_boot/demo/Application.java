ba0b2c1b5ce45e197cdf604d21ef1022c1396cbe
Leandro Martini <martinileandro12@gmail.com>
2025-05-01 12:36:06 -0300

    injeção de dependencia

--- spring_boot/src/main/java/com/spring_boot/demo/Application.java
+++ spring_boot/src/main/java/com/spring_boot/demo/Application.java
@@ -3,7 +3,7 @@ package com.spring_boot.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ spring_boot/src/main/java/com/spring_boot/repository/ClientesRepository.java
@@ -0,0 +1,13 @@
+package com.spring_boot.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring_boot.model.Cliente;
+
+@Repository
+public class ClientesRepository {
+    
+    public void persistir(Cliente cliente){
+        //salvar no banco de dados
+    }
+}
