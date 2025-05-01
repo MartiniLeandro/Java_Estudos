73d92fb6d8cd6c90b8317a048e7e5be02e1fda79
Leandro Martini <martinileandro12@gmail.com>
2025-05-01 18:48:25 -0300

    trabalhando com diferentes aplications e profiles

--- spring_boot/src/main/java/com/spring_boot/demo/MyConfiguration.java
+++ spring_boot/src/main/java/com/spring_boot/demo/MyConfiguration.java
@@ -1,13 +1,19 @@
 package com.spring_boot.demo;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
     
-    //@Bean(name="applicationName")
-    //public String applicationName(){
-    //    return "Sell System";
-    //}
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
