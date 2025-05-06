ecd39ed8e84064d880d414169a16b77ab73ac312
Leandro Martini <martinileandro12@gmail.com>
2025-05-06 19:52:44 -0300

    Order, Instant, ISO 8601 PT1

--- workshop-springboot/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ workshop-springboot/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -23,8 +23,8 @@ public class TestConfig implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com","97777777", "1234567");
+        User u1 = new User("Maria Brown", "maria@gmail.com", "98888888", "123456");
+        User u2 = new User("Alex Green", "alex@gmail.com","97777777", "1234567");
 
         userRepository.saveAll(Arrays.asList(u1,u2));
     }
--- workshop-springboot/src/main/java/com/educandoweb/course/models/User.java
+++ workshop-springboot/src/main/java/com/educandoweb/course/models/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.course.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,10 +23,12 @@ public class User implements Serializable /*Serializable serve para ser transfor
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User(){}
 
-    public User(Long id, String name, String email, String phone, String password) {
-        this.id = id;
+    public User(String name, String email, String phone, String password) {
         this.name = name;
         this.email = email;
         this.phone = phone;
@@ -68,6 +73,10 @@ public class User implements Serializable /*Serializable serve para ser transfor
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders(){
+        return this.orders;
     }
 
     @Override
