3b3fbcfaf8d88ffea81fddca7b1367d8ef0c4118
Leandro Martini <martinileandro12@gmail.com>
2025-05-09 21:58:17 -0300

    teste com professores

--- treino_springboot/src/main/java/com/spring_boot/config/TestConfig.java
+++ treino_springboot/src/main/java/com/spring_boot/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.spring_boot.models.Aluno;
+import com.spring_boot.models.Professor;
 import com.spring_boot.repository.AlunoRepository;
 import com.spring_boot.repository.ProfessorRepository;
 
@@ -32,7 +33,12 @@ public class TestConfig implements CommandLineRunner{
         Aluno a2 = new Aluno(null, "Gabriel", "gabriel@gmail.com", 22);
         Aluno a3 = new Aluno(null, "Thiago", "thiago@gmail.com", 14);
 
+        Professor p1 = new Professor(null, "Leandro", "leandro@gmail.com", 20);
+        Professor p2 = new Professor(null, "gabriel", "gabriel@gmail.com", 20);
+        Professor p3 = new Professor(null, "Thiago", "thiago@gmail.com", 20);
+
         alunoRepository.saveAll(Arrays.asList(a1,a2,a3));
+        professorRepository.saveAll(Arrays.asList(p1,p2,p3));
 
     }
 }
