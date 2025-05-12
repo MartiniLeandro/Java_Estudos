aae621f9f00d0888050075140cebc7fcc24c1bb3
Leandro Martini <martinileandro12@gmail.com>
2025-05-11 22:34:15 -0300

    criando GET dos professores

---
+++ treino_springboot/src/main/java/com/spring_boot/controllers/ProfessorController.java
@@ -0,0 +1,30 @@
+package com.spring_boot.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring_boot.models.Professor;
+import com.spring_boot.services.ProfessorService;
+
+@RestController
+@RequestMapping (value = "/professores")
+public class ProfessorController {
+    
+    private ProfessorService professorService;
+
+    public ProfessorController(ProfessorService professorService){
+        this.professorService = professorService; 
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Professor>> findAll(){
+        List<Professor> professores = professorService.findAll();
+        return ResponseEntity.ok().body(professores);
+    }
+
+}
---
+++ treino_springboot/src/main/java/com/spring_boot/services/ProfessorService.java
@@ -0,0 +1,17 @@
+package com.spring_boot.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spring_boot.models.Professor;
+
+@Service
+public class ProfessorService {
+    
+    private ProfessorService professorRepository;
+
+    public List<Professor> findAll(){
+        return professorRepository.findAll();
+    }
+}
