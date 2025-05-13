3e9b38b71b8e7dfdbe1b5741df6d57c6a5a9682f
Leandro Martini <martinileandro12@gmail.com>
2025-05-13 11:15:55 -0300

    estrutura básica do projeto

---
+++ SistemaBiblioteca_springboot/SistemaBiblioteca_springboot/src/main/java/com/SistemaBilioteca_springboot/SistemaBiblioteca_springboot/SistemaBibliotecaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaBilioteca_springboot.SistemaBiblioteca_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBibliotecaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBibliotecaSpringbootApplication.class, args);
+	}
+
+}
---
+++ SistemaBiblioteca_springboot/SistemaBiblioteca_springboot/src/test/java/com/SistemaBilioteca_springboot/SistemaBiblioteca_springboot/SistemaBibliotecaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemaBilioteca_springboot.SistemaBiblioteca_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
