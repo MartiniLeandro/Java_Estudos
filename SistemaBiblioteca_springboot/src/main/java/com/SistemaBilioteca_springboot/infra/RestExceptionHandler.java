5c8dc422bfe2b7e55bfdf5b6adfc44f587eb2c40
Leandro Martini <martinileandro12@gmail.com>
2025-05-17 18:21:03 -0300

    Organizando pastas

---
+++ SistemaBiblioteca_springboot/src/main/java/com/SistemaBilioteca_springboot/infra/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.SistemaBilioteca_springboot.infra;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.SistemaBilioteca_springboot.errors.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    private ResponseEntity<ErrorAPI> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        ErrorAPI error = new ErrorAPI(HttpStatus.NOT_FOUND.value(), "Not found", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ResponseEntity<ErrorAPI> ConstraintViolation(ConstraintViolationException e, HttpServletRequest request){
+        ErrorAPI error = new ErrorAPI(HttpStatus.BAD_REQUEST.value(), "Violation Contract", "Violação de Contrato", request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
