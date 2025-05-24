b5d2126aa742c380a787ba67b8b1d9e625a53927
Leandro Martini <martinileandro12@gmail.com>
2025-05-24 20:16:15 -0300

    arrumando projeto

--- treino-security-noJWT/src/main/java/com/treino_security_noJWT/repositories/UserRepository.java
+++ treino-security-noJWT/src/main/java/com/treino_security_noJWT/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.treino_security_noJWT.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long>{
+import com.treino_security_noJWT.Models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
     UserDetails findByLogin(String login);
 }
--- treino-security-noJWT/src/main/java/com/treino_security_noJWT/services/SecurityService.java
+++ treino-security-noJWT/src/main/java/com/treino_security_noJWT/services/SecurityService.java
@@ -3,9 +3,11 @@ package com.treino_security_noJWT.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.treino_security_noJWT.repositories.UserRepository;
 
+@Service
 public class SecurityService implements UserDetailsService{
 
     private UserRepository userRepository;
