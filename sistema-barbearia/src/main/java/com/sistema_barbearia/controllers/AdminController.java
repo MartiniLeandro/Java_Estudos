540147ea684735ad9b080c276196111334dd4e4b
Leandro Martini <martinileandro12@gmail.com>
2025-06-17 08:54:45 -0300

    Base do controller e service do ADMIN

--- sistema-barbearia/src/main/java/com/sistema_barbearia/controllers/AdminController.java
+++ sistema-barbearia/src/main/java/com/sistema_barbearia/controllers/AdminController.java
@@ -1,11 +1,16 @@
 package com.sistema_barbearia.controllers;
 
+import com.sistema_barbearia.entities.Barbeiro;
+import com.sistema_barbearia.entities.Cliente;
 import com.sistema_barbearia.entities.DTOS.agendamentosBarbeiro.AddAgendamentoBarbeiroDTO;
 import com.sistema_barbearia.entities.DTOS.agendamentosBarbeiro.DeleteAgendamentoBarbeiroDTO;
 import com.sistema_barbearia.entities.DTOS.agendamentosBarbeiro.UpdateAgendamentoBarbeiroDTO;
+import com.sistema_barbearia.entities.User;
 import com.sistema_barbearia.services.AdminService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -17,21 +22,76 @@ public class AdminController {
         this.adminService = adminService;
     }
 
-    @PostMapping("/add/agendamentos-barbeiro")
+    @PostMapping("/add/agendamentosBarbeiros")
     public ResponseEntity<String> addAgendamentoBarbeiro(@RequestBody AddAgendamentoBarbeiroDTO data){
         adminService.addAgendamentoBarbeiro(data.agendamento(), data.idBarbeiro());
         return ResponseEntity.ok().body("Agendamento adicionado ao barbeiro com ID: " + data.idBarbeiro());
     }
 
-    @DeleteMapping("/delete/agendamentos-barbeiro")
+    @DeleteMapping("/delete/agendamentosBarbeiros")
     public ResponseEntity<String> deleteAgendamentoBarbeiro(@RequestBody DeleteAgendamentoBarbeiroDTO data){
         adminService.deleteAgendamentoBarbeiro(data.index(), data.idBarbeiro());
         return ResponseEntity.ok().body("Agendamento deletado ao barbeiro com ID: " + data.idBarbeiro());
     }
 
-    @PutMapping("/update/agendamentos-barbeiro")
+    @PutMapping("/update/agendamentosBarbeiros")
     public ResponseEntity<String> updateAgendamentoBarbeiro(@RequestBody UpdateAgendamentoBarbeiroDTO data){
         adminService.updateAgendamentoBarbeiro(data.index(), data.agendamento(), data.idBarbeiro());
         return ResponseEntity.ok().body("Agendamento atualizado ao barbeiro com ID: " + data.idBarbeiro());
     }
+
+    @GetMapping("/allBarbeiros")
+    public ResponseEntity<List<Barbeiro>> findAllBarbeiros(){
+        List<Barbeiro> allBarbeiros = adminService.listAllBarbeiro();
+        return ResponseEntity.ok().body(allBarbeiros);
+    }
+
+    @DeleteMapping("/deleteBarbeiros/{id}")
+    public ResponseEntity<String> deleteBarbeiro(@PathVariable Long id){
+        adminService.deleteBarbeiro(id);
+        return ResponseEntity.ok().body("barbeiro deletado");
+    }
+
+    @PutMapping("/updateBarbeiros/{id}")
+    public ResponseEntity<Barbeiro> updateBarbeiro(@PathVariable  Long id, @RequestBody Barbeiro barbeiro){
+        Barbeiro barbeiroUpdate = adminService.updateBarbeiro(id, barbeiro);
+        return ResponseEntity.ok().body(barbeiroUpdate);
+    }
+
+    @GetMapping("/allClientes")
+    public ResponseEntity<List<Cliente>> findAllClientes(){
+        List<Cliente> allClientes = adminService.listAllCliente();
+        return ResponseEntity.ok().body(allClientes);
+    }
+
+    @DeleteMapping("/deleteClientes/{id}")
+    public ResponseEntity<String> deleteCliente(@PathVariable Long id){
+        adminService.deleteCliente(id);
+        return ResponseEntity.ok().body("cliente Deletado");
+    }
+
+    @PutMapping("/updateClientes/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable  Long id,@RequestBody Cliente cliente){
+        Cliente updateCliente = adminService.updateCliente(id, cliente);
+        return ResponseEntity.ok().body(updateCliente);
+    }
+
+    @GetMapping("/allUsers")
+    public ResponseEntity<List<User>> findAllUsers(){
+        List<User> allUsers = adminService.listAllUsers();
+        return ResponseEntity.ok().body(allUsers);
+    }
+
+    @DeleteMapping("/deleteUsers/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        adminService.deleteUser(id);
+        return ResponseEntity.ok().body("User deletado");
+    }
+
+    @PutMapping("/updateUsers/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
+        User updateUser = adminService.updateUser(id, user);
+        return ResponseEntity.ok().body(updateUser);
+    }
+
 }
--- sistema-barbearia/src/main/java/com/sistema_barbearia/services/AdminService.java
+++ sistema-barbearia/src/main/java/com/sistema_barbearia/services/AdminService.java
@@ -1,7 +1,12 @@
 package com.sistema_barbearia.services;
 
 import com.sistema_barbearia.entities.Barbeiro;
+import com.sistema_barbearia.entities.Cliente;
+import com.sistema_barbearia.entities.User;
 import com.sistema_barbearia.repositories.BarbeiroRepository;
+import com.sistema_barbearia.repositories.ClienteRepository;
+import com.sistema_barbearia.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +15,15 @@ import java.util.List;
 public class AdminService {
 
     private final BarbeiroRepository barbeiroRepository;
+    private final ClienteRepository clienteRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AdminService(BarbeiroRepository barbeiroRepository) {
+    public AdminService(BarbeiroRepository barbeiroRepository, ClienteRepository clienteRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.barbeiroRepository = barbeiroRepository;
+        this.clienteRepository = clienteRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void addAgendamentoBarbeiro(String agendamento, Long idBarbeiro){
@@ -36,4 +47,54 @@ public class AdminService {
         barbeiroRepository.save(barbeiro);
 
     }
+
+    public List<Barbeiro> listAllBarbeiro(){
+        return barbeiroRepository.findAll();
+    }
+
+    public void deleteBarbeiro(Long id){
+        barbeiroRepository.deleteById(id);
+    }
+
+    public Barbeiro updateBarbeiro(Long id, Barbeiro barbeiro){
+        Barbeiro updateBarbeiro = barbeiroRepository.findById(id).orElseThrow(() -> new RuntimeException("não há barbeiro com este ID"));
+        updateBarbeiro.setInicioTrabalho(barbeiro.getInicioTrabalho());
+        updateBarbeiro.setFinalTrabalho(barbeiro.getFinalTrabalho());
+        updateBarbeiro.setUser(barbeiro.getUser());
+
+        return barbeiroRepository.save(updateBarbeiro);
+    }
+
+    public List<Cliente> listAllCliente(){
+        return clienteRepository.findAll();
+    }
+
+    public void deleteCliente(Long id){
+        clienteRepository.deleteById(id);
+    }
+
+    public Cliente updateCliente(Long id, Cliente cliente){
+        Cliente updateCliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("não há barbeiro com este ID"));
+        updateCliente.setTelefone(cliente.getTelefone());
+        updateCliente.setUser(cliente.getUser());
+        return clienteRepository.save(updateCliente);
+    }
+
+    public List<User> listAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(Long id, User user){
+        User updateUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("não há user com este ID"));
+        updateUser.setEmail(user.getEmail());
+        updateUser.setNome(user.getNome());
+        updateUser.setSenha(passwordEncoder.encode(user.getSenha()));
+
+        return userRepository.save(updateUser);
+    }
+
 }
