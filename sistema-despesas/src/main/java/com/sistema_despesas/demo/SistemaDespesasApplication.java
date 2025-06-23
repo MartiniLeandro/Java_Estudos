e6d2af9dfffdd3ea83800e431017b7c811105962
Leandro Martini <martinileandro12@gmail.com>
2025-06-23 08:23:52 -0300

    estrutura novo projeto

---
+++ sistema-despesas/src/main/java/com/sistema_despesas/demo/SistemaDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_despesas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDespesasApplication.class, args);
+	}
+
+}
---
+++ sistema-despesas/src/test/java/com/sistema_despesas/demo/SistemaDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_despesas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
