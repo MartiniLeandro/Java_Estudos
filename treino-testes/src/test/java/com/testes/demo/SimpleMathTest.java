a0d385d5514eff17b4c9bd741519cb55fa9aec87
Leandro Martini <martinileandro12@gmail.com>
2025-07-01 19:16:19 -0300

    @BeforeAll, @BeforeEach, @AfterAll, @AfterEach

--- treino-testes/src/test/java/com/testes/demo/SimpleMathTest.java
+++ treino-testes/src/test/java/com/testes/demo/SimpleMathTest.java
@@ -1,21 +1,44 @@
 package com.testes.demo;
 
 import com.testes.demo.Classes.SimpleMath;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Fail.fail;
 
 public class SimpleMathTest {
 
-    SimpleMath math = new SimpleMath();
+    SimpleMath math;
     String message = "O resultado esperado não é esse";
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("Executando @BeforeAll");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+         math = new SimpleMath();
+        System.out.println("Executando @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("Executando @AfterEach");
+    }
+
+
+    @AfterAll
+    static void cleanup(){
+        System.out.println("Executando @AfterAll");
+    }
+
 
     @Test
     @DisplayName("Test 6.2 + 2.0 = 8.2")
     void testSoma_SeisPontoDoisMaisDois_DeveRetornarOitoPontoDois(){
+
+        System.out.println("Executando teste1");
         //GIVEN
         Double firstNumber = 6.2;
         Double secondNumber = 2.0;
@@ -30,6 +53,7 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test 8.0 - 4.0 = 4.0")
     void TestSubtraction_OitoMenosQuatro_DeveRetornarQuatro(){
+        System.out.println("Executando teste2");
         Double result = math.subtraction(8.0,4.0);
         Assertions.assertEquals(4.0, result, () -> message);
     }
@@ -37,6 +61,7 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Test 5.0 * 2.0 | 20.0 / 10.0 | RaizQuadrada de 124")
     void TestDivisionMediaRaizQuadrada(){
+        System.out.println("Executando teste3");
         Double result = math.multiplication(5.0,2.0);
         Assertions.assertEquals(10.0,result, () -> message);
 
@@ -50,6 +75,7 @@ public class SimpleMathTest {
         Assertions.assertEquals(11.135528725660043, resultSquareRoot, () -> message);
     }
 
+    @Disabled
     @Test
     @DisplayName("Divisão por zero")
     void TestDivision_PrimeiroNumeroPorZero_DeveRetornarArithmeticalException(){
