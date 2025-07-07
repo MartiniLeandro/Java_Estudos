5a994ee68dcdafc9aae78fb6cf75d2e875272b3f
Leandro Martini <martinileandro12@gmail.com>
2025-07-07 07:03:58 -0300

    todos test para o service

--- SpringBootWithTest/demo/src/test/java/com/projectWithTest/demo/UserServiceTest.java
+++ SpringBootWithTest/demo/src/test/java/com/projectWithTest/demo/UserServiceTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -34,7 +33,7 @@ public class UserServiceTest {
         user = new User("Leandro", "Martini","SC","Male","leandro@email.com");
     }
 
-    @DisplayName("test save user to db SUCCESS")
+    @DisplayName("test save user SUCCESS")
     @Test
     void testSaveUserSuccess(){
 
@@ -45,7 +44,7 @@ public class UserServiceTest {
         Assertions.assertEquals(savedUser,user);
     }
 
-    @DisplayName("test save user to db FAILED")
+    @DisplayName("test save user FAILED")
     @Test
     void testSaveUserFailed(){
 
@@ -55,7 +54,7 @@ public class UserServiceTest {
         Mockito.verify(userRepository, Mockito.never()).save(Mockito.any());
     }
 
-    @DisplayName("test find all to db SUCCESS")
+    @DisplayName("test find all UCCESS")
     @Test
     void testFindAllSuccess(){
 
@@ -66,7 +65,7 @@ public class UserServiceTest {
         Assertions.assertEquals(1, allUsers.size());
     }
 
-    @DisplayName("test find all to db FAILED")
+    @DisplayName("test find all FAILED")
     @Test
     void testFindAllFailed(){
 
@@ -77,15 +76,75 @@ public class UserServiceTest {
         Assertions.assertEquals(0, allUsers.size());
     }
 
-    @DisplayName("test find by id to db")
+    @DisplayName("test find by id SUCCESS")
     @Test
-    void testFindById(){
+    void testFindByIdSuccess(){
 
         Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
         User userFindId = userService.findById(1L);
 
         Assertions.assertNotNull(userFindId);
         Assertions.assertEquals(userFindId,user);
+    }
+
+    @DisplayName("test find by id FAILED")
+    @Test
+    void testFindByIdFailed(){
+
+        Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+
+
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class,() -> userService.findById(1L));
+        Assertions.assertEquals("Não existe user com este ID", exception.getMessage());
+    }
+
+    @DisplayName("test update User SUCCESS")
+    @Test
+    void testUpdateUserSuccess(){
+
+        Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
+        user.setFirstName("Leonardo");
+        user.setEmail("leonardo@email.com");
+        user.setLastName("Martini");
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        User updateUser = userService.update(user,1L);
+
+        Assertions.assertNotNull(updateUser);
+        Assertions.assertEquals("leonardo@email.com", updateUser.getEmail());
+        Assertions.assertEquals(updateUser, user);
+    }
+
+    @DisplayName("test update User FAILED")
+    @Test
+    void testUpdateUserFailed(){
+
+        Mockito.when(userRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        RuntimeException exception =
+                Assertions.assertThrows(RuntimeException.class, () -> userService.update(user, 1L));
+                Assertions.assertEquals("Não existe user com este ID", exception.getMessage());
+    }
+
+    @DisplayName("test delete User SUCCESS")
+    @Test
+    void testDeleteUserSuccess(){
+
+        userService.delete(1L);
+        Mockito.verify(userRepository).deleteById(1L);
+    }
+
+    @DisplayName("test delete User FAILED")
+    @Test
+    void testDeleteUserFailed(){
+
+        Mockito.doThrow(new RuntimeException("Erro ao deletar")).when(userRepository).deleteById(1L);
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {userService.delete(1L);
+        });
+
+        Assertions.assertEquals("Erro ao deletar", exception.getMessage());
     }
 
 
