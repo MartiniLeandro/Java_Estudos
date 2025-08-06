49d9f9ffae13c83528000526c78025bb9d9e73e8
Leandro Martini <martinileandro12@gmail.com>
2025-08-06 20:15:06 -0300

    config do teste de integração, e primeiro teste feito

---
+++ SpringBootWithTest/demo/src/test/java/com/projectWithTest/demo/integrationTests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.projectWithTest.demo.integrationTests.swagger;
+
+import com.projectWithTest.demo.config.AbstractIntegrationTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setupRestAssured() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("Teste deve mostrar a página do Swagger")
+    @Test
+    void testReturnSwaggerPage() {
+        String content = RestAssured.given()
+                .basePath("/swagger-ui/index.html")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .asString();
+
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
