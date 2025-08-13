e9fdae6d1daf3876e2b9dd3d192b90c7aeb18c70
Leandro Martini <martinileandro12@gmail.com>
2025-08-13 08:44:00 -0300

    adicionando configs para o swagger

--- SpringBootWithTest/demo/src/main/java/com/projectWithTest/demo/controllers/UserController.java
+++ SpringBootWithTest/demo/src/main/java/com/projectWithTest/demo/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.projectWithTest.demo.controllers;
 
 import com.projectWithTest.demo.entities.User;
 import com.projectWithTest.demo.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +21,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(description = "Deve retornar todos os Users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todos usuários encontrados"),
+            @ApiResponse(responseCode = "400", description = "Falha ao encontrar Usuários")
+    })
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
         return ResponseEntity.ok().body(userService.findAll());
     }
 
+    @Operation(description = "Deve retornar o User pelo ID informado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User encontrado"),
+            @ApiResponse(responseCode = "400", description = "Não existe User com este Id")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         try{
@@ -31,11 +45,21 @@ public class UserController {
         }
     }
 
+    @Operation(description = "Deve criar um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "usuário criado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar usuário")
+    })
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User user){
         return ResponseEntity.ok().body(userService.create(user));
     }
 
+    @Operation(description = "Deve atualizar um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "usuário atualizado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar usuário")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<User> update(@RequestBody User user, @PathVariable Long id){
         try{
@@ -45,6 +69,11 @@ public class UserController {
         }
     }
 
+    @Operation(description = "Deve excluir um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "usuário excluído"),
+            @ApiResponse(responseCode = "400", description = "Erro ao excluir usuário")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         userService.delete(id);
