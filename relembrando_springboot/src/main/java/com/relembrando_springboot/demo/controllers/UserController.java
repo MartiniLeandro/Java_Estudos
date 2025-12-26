5ff55e549c886dd4993a2346386d59590c1dc3c4
Leandro Martini <martinileandro12@gmail.com>
2025-12-26 20:32:22 -0300

    Controller User sem muitas configs

--- relembrando_springboot/src/main/java/com/relembrando_springboot/demo/controllers/UserController.java
+++ relembrando_springboot/src/main/java/com/relembrando_springboot/demo/controllers/UserController.java
@@ -3,9 +3,9 @@ package com.relembrando_springboot.demo.controllers;
 import com.relembrando_springboot.demo.entities.User;
 import com.relembrando_springboot.demo.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +18,29 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<User>
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok().body(userService.allUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok().body(userService.userById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return ResponseEntity.ok().body(userService.createUser(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
+        return ResponseEntity.ok().body(userService.updateUser(user,id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
