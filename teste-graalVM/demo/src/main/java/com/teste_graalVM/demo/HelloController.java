5a56cc14fb778293417c7dda59a34fc8c500be45
Leandro Martini <martinileandro12@gmail.com>
2026-09-15 20:31:37 -0300

    criando docker para projetos teste e criando docker compose para estes projetos

--- teste-graalVM/demo/src/main/java/com/teste_graalVM/demo/HelloController.java
+++ teste-graalVM/demo/src/main/java/com/teste_graalVM/demo/HelloController.java
@@ -1,4 +1,16 @@
 package com.teste_graalVM.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> helloGraalVM(){
+        return ResponseEntity.ok("Hello, GraalVM!!");
+    }
 }
